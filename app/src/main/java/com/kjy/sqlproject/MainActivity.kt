e6baf4b1869e8e0f60f6323e556f5c07924a746a
JaeYeon-Kim <example@example.com>
2022-03-22 20:54:20 +0900

    Feat: "Add 삭제 기능, 메모 내용 전체보기"

--- app/src/main/java/com/kjy/sqlproject/MainActivity.kt
+++ app/src/main/java/com/kjy/sqlproject/MainActivity.kt
@@ -2,10 +2,57 @@ package com.kjy.sqlproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kjy.sqlproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    // SqliteHelper를 생성하고 변수에 저장
+    val helper = SqliteHelper(this, "memo", 1)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        // RecyclerAdapter를 생성
+        val adapter = RecyclerAdapter()
+
+        // helper를 어댑터에 전달해줌
+        adapter.helper = helper
+
+        // adapter의 listData에 데이터베이스에서 가져온 데이터를 셋팅
+        adapter.listData.addAll(helper.selectMemo())
+
+        // 화면의 리사이클러뷰 위젯에 adapter를 연결하고 레이아웃 매니저를 설정
+        binding.recyclerMemo.adapter = adapter
+        binding.recyclerMemo.layoutManager = LinearLayoutManager(this)
+
+        // 저장 버튼에 클릭리스너를 연결
+        binding.buttonSave.setOnClickListener {
+            // 메모를 입력하는 플레인텍스트를 검사해서 값이 있으면 해당 내용으로 Memo 클래스를 생성
+            if (binding.editMemo.text.toString().isNotEmpty()) {
+                val memo = Memo(null, binding.editMemo.text.toString(), System.currentTimeMillis())
+
+                // helper 클래스의 insertMemo() 메서드 앞에서 생성한 Memo를 전달해 데이터베이스에 저장
+                helper.insertMemo(memo)
+
+                // 어댑터 데이터 초기화
+                adapter.listData.clear()
+
+                // 데이터베이스에서 새로운 목록을 읽어와 어댑터에 세팅하고 갱신함.
+                adapter.listData.addAll(helper.selectMemo())
+                // 새로 생성되는 메모에는 번호가 자동 입력되므로 번호를 갱신하기 위해서 새로운 데이터를 셋팅
+                adapter.notifyDataSetChanged()
+
+                // 메모 내용을 입력하는 위젯의 내용을 지워서 초기화
+                binding.editMemo.setText("")
+            }
+        }
+
+
     }
 }
--- app/src/main/java/com/kjy/sqlproject/RecyclerAdapter.kt
+++ app/src/main/java/com/kjy/sqlproject/RecyclerAdapter.kt
@@ -9,7 +9,10 @@ import java.text.SimpleDateFormat
 
 // RecyclerAdapter 만들기
 
-class RecyclerAdapter: RecyclerView.Adapter<Holder>() {
+class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.Holder>() {
+    // helper 프로퍼티 생성
+    var helper: SqliteHelper? = null
+
     var listData = mutableListOf<Memo>()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val binding = ItemRecyclerBinding.inflate(LayoutInflater.from(parent.context),
@@ -28,16 +31,36 @@ class RecyclerAdapter: RecyclerView.Adapter<Holder>() {
         holder.setMemo(memo)
     }
 
+    // Holder 클래스 생성
+    // 기존의 Holder클래스를 어댑터 밖에 선언했기 때문에 어댑터 안쪽으로 옮겨주고 inner 클래스로 선언하여
+    // 어댑터의 멤버 변수에 접근할 수 있도록 함.
+    // 홀더는 한 화면에 그려지는 개수만큼 만든 후 재사용하므로 1번 메모가 있는 홀더를 스크롤해서 위로 올리면 아래에서 올라오는
+    // 새로운 메모가 1번 홀더를 재사용 하는 구조이기 때문에 클릭 하는 시점에 어떤 데이터가 있는지 알아야 함.
+    inner class Holder(val binding: ItemRecyclerBinding): RecyclerView.ViewHolder(binding.root) {
+        // setMemo() 메서드로 넘어온 Memo를 임시저장
+        var mMemo: Memo? = null
 
-}
+        init {
+            binding.buttonDelete.setOnClickListener {
+                // SQLite의 데이터를 먼저 삭제하고, listData의 데이터도 삭제하고 , 어댑터를 갱신함.
+                // deleteMemo() 는 null을 허용하지 않지만, mMemo는 null을 허용하도록 설정되었기때문에
+                // !!를 사용해서 강제적으로 해야한다.
+                helper?.deleteMemo(mMemo!!)
+                listData.remove(mMemo)
+                notifyDataSetChanged()
 
-// Holder 클래스 생성
-class Holder(val binding: ItemRecyclerBinding): RecyclerView.ViewHolder(binding.root) {
-    fun setMemo(memo: Memo) {
-        binding.textNo.text = "${memo.no}"
-        binding.textContent.text = memo.content
-        val sdf = SimpleDateFormat("yyyy/MM/dd hh:mm")
-        // 날짜 포맷은 SimpleDateFormat으로 설정
-        binding.textDatetime.text = "${sdf.format(memo.datetime)}"
+
+            }
+        }
+        fun setMemo(memo: Memo) {
+            binding.textNo.text = "${memo.no}"
+            binding.textContent.text = memo.content
+            val sdf = SimpleDateFormat("yyyy/MM/dd hh:mm")
+            // 날짜 포맷은 SimpleDateFormat으로 설정
+            binding.textDatetime.text = "${sdf.format(memo.datetime)}"
+            this.mMemo = memo
+        }
     }
+
+
 }
